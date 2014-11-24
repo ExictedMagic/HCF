913ef04963c315742b31bb9a44b185872a33b517
暖阳丶午后 <s_pmsun@163.com>
2014-11-24 22:51:31 +0800

    增加文件后缀名常量设置

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * 入口文件
+ * Author: pmsun
  */
 //监测php版本
 if (version_compare(PHP_VERSION, '5.3.0', '<')) exit('请升级PHP到5.3以上版本');
 define('DEBUG', 'on');
+define('EXT', '.php');
 //定义框架的根目录
 define('ROOT_PATH', __DIR__);
 require ROOT_PATH . '/core/Bootstrap.php';
