4b4f8dd01e6bb28c8e76614dcc81def42cf384a8
暖阳丶午后 <s_pmsun@163.com>
2014-11-24 22:59:31 +0800

    增加自动加载，路由的实例化

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -2,7 +2,15 @@
 /**
  * Author: pmsun
  */
+require __DIR__ . "/common/function.php";
+//注册自动加载函数
+spl_autoload_register('autoload');
+
 require __DIR__ . "/hcf/Hcf.php";
 
 global $hcf;
 $hcf = Hcf::getInstance();
+
+//加载路由类
+import('Router', 'hcf');
+$hcf->router = new Router();
